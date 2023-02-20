9dde40740177770f68385510a4ee263fa3492549
劉知穎 <liuzhiying@DN0a1ea039.SUNet>
2023-02-19 19:53:11 -0800

    script pretrain

--- scripts/train_p1.sh
+++ scripts/train_p1.sh
@@ -4,3 +4,8 @@
 python classifier.py --option finetune \
         --epochs 10 --lr 1e-5 --batch_size 8\
         --hidden_dropout_prob 0.3
+
+# Pretrain the model
+python classifier.py --option pretrain \
+		--epochs 10 --lr 1e-3 --batch_size 8\
+		--hidden_dropout_prob 0.3
