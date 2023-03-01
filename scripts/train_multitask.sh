4c3c70265a68716b49da3e2802a482df72436a8b
Ubuntu <ubuntu@ip-172-31-7-62.us-west-2.compute.internal>
2023-03-01 04:31:29 +0000

    training results, 6 epochs w/2 linear layers + ReLU

--- scripts/train_multitask.sh
+++ scripts/train_multitask.sh
@@ -3,6 +3,6 @@
 # Finetune the model
 python multitask_classifier.py --option finetune --use_gpu\
 	--output_dir result/tmpJeff \
-    --epochs 10 --lr 1e-5 --batch_size 16\
+    --epochs 6 --lr 1e-5 --batch_size 16\
     --hidden_dropout_prob 0.3\
     --sample rr
