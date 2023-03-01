495b1ad1d66676445b595967c8c14ab94bce8355
Jeffery Shen <jefferyshen@MacBook-Air-39.local>
2023-02-28 16:33:02 -0800

    multitask head editing

--- scripts/train_multitask.sh
+++ scripts/train_multitask.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # Finetune the model
-python multitask_classifier.py --option pretrain\
-	--output_dir result/tmp \
-    --epochs 6 --lr 1e-3 --batch_size 16 \
+python multitask_classifier.py --option finetune --use_gpu\
+	--output_dir result/tmpJeff \
+    --epochs 10 --lr 1e-5 --batch_size 16\
     --hidden_dropout_prob 0.3\
-    --sample rr --concat_pair
+    --sample rr
