1a763a9616f151e0d653da202babc37fd310e66f
grcgs2212 <ying1029@stanford.edu>
2023-03-01 02:57:55 +0000

    train concat_pair

--- scripts/train_multitask.sh
+++ scripts/train_multitask.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # Finetune the model
-python multitask_classifier.py --option pretrain\
-	--output_dir result/tmp \
+python multitask_classifier.py --option finetune --use_gpu\
+	--output_dir result/finetune_concat_baseline \
     --epochs 6 --lr 1e-3 --batch_size 16 \
     --hidden_dropout_prob 0.3\
     --sample rr --concat_pair
