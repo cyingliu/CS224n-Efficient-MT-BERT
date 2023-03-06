dff226a388a9dc888a76bf9be333e1c7f8cd0fd0
Ubuntu <ubuntu@ip-172-31-7-62.us-west-2.compute.internal>
2023-03-06 22:31:56 +0000

    prefix pretrain results

--- scripts/train_multitask.sh
+++ scripts/train_multitask.sh
@@ -2,8 +2,8 @@
 
 # Finetune the model
 python multitask_classifier.py --option pretrain --use_gpu\
-	--output_dir result/prefix_pretrain_20_lr4e-3\
-    --epochs 25 --lr 4e-3 --batch_size 16 --steps_per_epoch 2400 --eval_interval 4\
+	--output_dir result/prefix_pretrain_10_lr1e-3\
+    --epochs 25 --lr 1e-3 --batch_size 16 --steps_per_epoch 2400 --eval_interval 4\
     --gradient_accumulation_step 1\
     --hidden_dropout_prob 0.3\
     --sample anneal\
