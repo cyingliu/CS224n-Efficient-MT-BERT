1a1fcb847e340cf08ad2de5d86634ad2deb3f93b
grcgs2212 <ying1029@stanford.edu>
2023-03-06 05:05:24 +0000

    pal result

--- scripts/train_multitask.sh
+++ scripts/train_multitask.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # Finetune the model
-python multitask_classifier.py --option pretrain\
-	--output_dir result/tmp\
+python multitask_classifier.py --option pretrain --use_gpu\
+	--output_dir result/pal_pretrain_share_204\
     --epochs 25 --lr 1e-5 --batch_size 16 --steps_per_epoch 2400 --eval_interval 4\
     --gradient_accumulation_step 1\
     --hidden_dropout_prob 0.3\
     --sample anneal\
-    --config_path config/houlsby_config.json
+    --config_path config/pal_config.json
