0a82e67cdb97e510bb6b63c0e5bf4713b943a612
Ubuntu <ubuntu@ip-172-31-7-62.us-west-2.compute.internal>
2023-03-06 05:17:55 +0000

    prefix finetune result

--- scripts/train_multitask.sh
+++ scripts/train_multitask.sh
@@ -2,9 +2,9 @@
 
 # Finetune the model
 python multitask_classifier.py --option pretrain --use_gpu\
-	--output_dir result/pal_pretrain_share_204\
+	--output_dir result/prefix_pretrain_20\
     --epochs 25 --lr 1e-5 --batch_size 16 --steps_per_epoch 2400 --eval_interval 4\
     --gradient_accumulation_step 1\
     --hidden_dropout_prob 0.3\
     --sample anneal\
-    --config_path config/pal_config.json
+    --config_path config/prefixl_config.json
