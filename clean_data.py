8cd74f85ed66078b7d92a79887ff3ebddf29a831
Jeffery Shen <jefferyshen@MacBook-Air-39.local>
2023-03-07 15:07:48 -0800

    new data cleaned

--- clean_data.py
+++ clean_data.py
@@ -3,10 +3,12 @@ import pandas as pd
 # read the input file using pandas
 df = pd.read_csv('SICK_train.txt', sep='	', header=0)
 
-# drop the first and last columns
+#df.insert(0, 'pair_ID_duplicate', df['pair_ID'])
+
+# drop the last columns
 df = df.iloc[:, :-1]
 
 df.columns = ['id', 'sentence1', 'sentence2', 'similarity']
               
 # write the modified dataframe to a new text file
-df.to_csv('SICK_train_cleaned.txt', sep='	', index=False)
+df.to_csv('SICK_train_cleaned.txt', sep='	', index=True)
