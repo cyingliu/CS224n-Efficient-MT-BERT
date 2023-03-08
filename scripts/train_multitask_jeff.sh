0d9e4d514872bf4b9449b73ce185e70f014f5a40
Ubuntu <ubuntu@ip-172-31-7-62.us-west-2.compute.internal>
2023-03-08 07:01:43 +0000

    palDoubleLayer results

--- scripts/train_multitask_jeff.sh
+++ scripts/train_multitask_jeff.sh
@@ -3,10 +3,10 @@
 # Finetune the model
 python multitask_classifier.py --option finetune --use_gpu\
 	--output_dir result/palDoubleLayer\
-  --epochs 25 --lr 1e-5 --batch_size 16 --steps_per_epoch 2400 --eval_interval 4\
+  --epochs 25 --lr 1e-5 --batch_size 16 --steps_per_epoch 2400 --eval_interval 8\
   --gradient_accumulation_step 1\
   --hidden_dropout_prob 0.1\
   --sample anneal\
   --config_path config/pal_config.json\
-  --lr_adapt 1e-3\
+  --lr_adapt 1e-5\
   --warmup_portion .1
