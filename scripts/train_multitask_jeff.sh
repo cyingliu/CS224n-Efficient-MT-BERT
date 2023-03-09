6cda9382897db83850fe35d8524767422d109212
Ubuntu <ubuntu@ip-172-31-7-62.us-west-2.compute.internal>
2023-03-09 02:23:48 +0000

    hyperparm tests

--- scripts/train_multitask_jeff.sh
+++ scripts/train_multitask_jeff.sh
@@ -2,7 +2,7 @@
 
 # Finetune the model
 python multitask_classifier.py --option finetune --use_gpu\
-	--output_dir result/palDoubleLayer\
+	--output_dir result/palDoubleLayer6Heads408Size\
   --epochs 25 --lr 1e-5 --batch_size 16 --steps_per_epoch 2400 --eval_interval 8\
   --gradient_accumulation_step 1\
   --hidden_dropout_prob 0.1\
