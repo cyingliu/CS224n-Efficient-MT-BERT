f8aad622734083cd4a033534c14cec93e4a24c1d
Ubuntu <ubuntu@ip-172-31-7-62.us-west-2.compute.internal>
2023-03-13 04:36:53 +0000

    best results so far w/ SST+STS additional data

--- scripts/train_multitask.sh
+++ scripts/train_multitask.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 # Finetune the model
-python multitask_classifier.py --option finetune \
-	--output_dir result/palDoubleLayer\
-    --epochs 25 --lr 1e-5 --batch_size 16 --steps_per_epoch 2400\
+python multitask_classifier.py --option finetune --use_gpu\
+	--output_dir result/multiheadBaseDataTests\
+    --epochs 25 --lr 1e-5 --batch_size 16 --steps_per_epoch 2400 --eval_interval 5\
     --gradient_accumulation_step 1\
     --hidden_dropout_prob 0.1\
     --sample anneal\
-    --config_path config/pal_config.json
