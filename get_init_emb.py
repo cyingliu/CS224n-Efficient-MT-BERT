c53b81516593dc9fdeaa4dd935e52e74131062bf
Jeffery Shen <jefferyshen@MacBook-Air-95.local>
2023-03-15 00:34:23 -0700

    new get init

--- get_init_emb.py
+++ get_init_emb.py
@@ -10,37 +10,14 @@ from bert import BertModel
 from config import BertConfig
 from datasets import SentenceClassificationDataset, SentencePairDataset, \
     load_multitask_data, load_multitask_test_data
-from multitask_classifier import MultitaskBERT
+from multitask_classifier import MultitaskBERT, get_args
 import json
 from tokenizer import BertTokenizer
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--sst_train", type=str, default="data/ids-sst-train.csv")
-    parser.add_argument("--sst_dev", type=str, default="data/ids-sst-dev.csv")
-    parser.add_argument("--sst_test", type=str, default="data/ids-sst-test-student.csv")
-
-    parser.add_argument("--para_train", type=str, default="data/quora-train.csv")
-    parser.add_argument("--para_dev", type=str, default="data/quora-dev.csv")
-    parser.add_argument("--para_test", type=str, default="data/quora-test-student.csv")
-
-    parser.add_argument("--sts_train", type=str, default="data/sts-train.csv")
-    parser.add_argument("--sts_dev", type=str, default="data/sts-dev.csv")
-    parser.add_argument("--sts_test", type=str, default="data/sts-test-student.csv")
-    
-    parser.add_argument("--use_gpu", action='store_true')
-
-    # hyper parameters
-    parser.add_argument("--batch_size", help='sst: 64, cfimdb: 8 can fit a 12GB GPU', type=int, default=8)
-    parser.add_argument("--hidden_dropout_prob", type=float, default=0.3)
-    parser.add_argument("--lr", type=float, help="learning rate, default lr for 'pretrain': 1e-3, 'finetune': 1e-5",
-                        default=1e-5)
-    parser.add_argument("--config_path", help='config (.json) file for adaptation modules', type=str, default="config/prefix_getinit_config.json")
-    args = parser.parse_args()
-    return args
 
 if __name__ == "__main__":
     args = get_args()
+    args.config_path = 'config/prefix_getinit_config.json'
     device = torch.device('cuda') if args.use_gpu else torch.device('cpu')
     
     sst_dev_data, num_labels,para_dev_data, sts_dev_data = load_multitask_data(args.sst_dev,args.para_dev,args.sts_dev, split ='train')
@@ -53,9 +30,13 @@ if __name__ == "__main__":
               'num_labels': len(num_labels),
               'hidden_size': 768,
               'data_dir': '.',
-              'option': 'finetune',
-              'concat_pair': False,
-              'config_path': args.config_path}
+              'option': args.option,
+              'concat_pair': args.concat_pair,
+              'config_path': args.config_path,
+              'downstream': args.downstream,
+              'similarity_classifier_type': args.similarity_classifier_type,
+              'pooling_type': args.pooling_type,
+              'classification_concat_type': args.classification_concat_type}
     config = SimpleNamespace(**config)
 
     model = MultitaskBERT(config)
@@ -65,4 +46,3 @@ if __name__ == "__main__":
     b_ids, b_mask, b_labels = (batch['token_ids'], batch['attention_mask'], batch['labels'])
     b_ids, b_mask, b_labels = b_ids.to(device), b_mask.to(device), b_labels.to(device)
     logits = model.predict_sentiment(b_ids, b_mask)
-
