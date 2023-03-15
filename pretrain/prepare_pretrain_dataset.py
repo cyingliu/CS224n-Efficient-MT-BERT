2a8e908076d6a0d7bcd5aae9fe87e28f04d45002
grcgs2212 <ying1029@stanford.edu>
2023-03-14 23:51:30 -0700

    pretrain

--- pretrain/prepare_pretrain_dataset.py
+++ pretrain/prepare_pretrain_dataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 
 
 def prepare_sst_texts(file_path):
@@ -6,20 +7,45 @@ def prepare_sst_texts(file_path):
 	df = pd.read_csv(file_path , sep='\t')
 	sents = []
 	for sent in df['sentence']:
-		sents.append(sent)
+		sents.append(sent.strip())
+	print(f"Data size: {len(sents)}")
+	return sents
+
+def prepare_sentence_pair_texts(file_path):
+	print(f"Reading from {file_path}")
+	df = pd.read_csv(file_path , sep='\t')
+	sents = []
+	for sent in df['sentence1']:
+		try:
+			sents.append(sent.strip())
+		except:
+			pass # nan
+	for sent in df['sentence2']:
+		try:
+			sents.append(sent.strip())
+		except:
+			pass # nan
 	print(f"Data size: {len(sents)}")
 	return sents
 
 def write_to_file(sents, output_path):
 	print(f"Writing to {output_path}")
 	fout = open(output_path, 'w')
+	fout.write("text\n")
 	for sent in sents:
 		fout.write(sent + '\n')
 	fout.close()
+	print(f"Data size: {len(sents)}")
 
 if __name__ == "__main__":
-	output_path = 'data/sst-pretrain.csv'
-	sst_file_path = '../data/ids-sst-train.csv'
+	output_path = 'data/sst-quora-sts-pretrain-test.csv'
+	sst_file_path = '../data/ids-sst-dev.csv'
+	quora_file_path = '../data/quora-dev.csv'
+	sts_file_path = '../data/sts-dev.csv'
 	sents = []
 	sents.extend(prepare_sst_texts(sst_file_path))
+	sents.extend(prepare_sentence_pair_texts(quora_file_path))
+	sents.extend(prepare_sentence_pair_texts(sts_file_path))
+	# do shuffling after constructing dataset, so that the same chunk mostly contains close sentences
+
 	write_to_file(sents, output_path)
--- pretrain/pretrain.py
+++ pretrain/pretrain.py
@@ -1,7 +1,88 @@
+import os, sys
+sys.path.append('..')
+import argparse
 from tokenizer import BertTokenizer
+from bert import BertModel
+from datasets import load_dataset # hugging face
+from transformers import DataCollatorForLanguageModeling, TrainingArguments
 
+def tokenize_function(examples):
+    result = tokenizer(examples["text"])
+    if tokenizer.is_fast:
+        result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))]
+    return result
 
-
+def group_texts(examples):
+    # Concatenate all texts
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    # Compute length of concatenated texts
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the last chunk if it's smaller than chunk_size
+    total_length = (total_length // chunk_size) * chunk_size
+    # Split by chunks of max_len
+    result = {
+        k: [t[i : i + chunk_size] for i in range(0, total_length, chunk_size)]
+        for k, t in concatenated_examples.items()
+    }
+    # Create a new labels column
+    result["labels"] = result["input_ids"].copy()
+    return result
 
 if __name__ == "__main__":
-	model = BertModel.from_pretrained('bert-base-uncased')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_data_path', default='./data/sst-quora-sts-pretrain-train.csv')
+    parser.add_argument('--test_data_path', default='./data/sst-quora-sts-pretrain-test.csv') 
+    parser.add_argument('--output_dir', require=True)
+    args = parser.parse_args()
+
+
+    model = BertModel.from_pretrained('bert-base-uncased')
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    
+    # load train, test dataset texts
+    dataset = load_dataset("csv", data_files={'train': args.train_data_path, 'test': args.test_data_path}, delimiter='\t') # 'text'
+
+    # tokenize text
+    tokenized_datasets = dataset.map(tokenize_function, batched=True, remove_columns=["text"]) # 'input_ids', 'token_type_ids', 'attention_mask'
+    
+    # concatentate and chop data into length 128 chunks
+    chunk_size = 128
+    lm_datasets = tokenized_datasets.map(group_texts, batched=True)
+    # shuffle dataset
+    lm_datasets = lm_datasets.shuffle(seed=42)
+    
+    # random masking for language modeling
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
+
+    batch_size = 16 # 64
+    # Show the training loss with every epoch
+    logging_steps = len(lm_datasets["train"]) // batch_size
+
+    training_args = TrainingArguments(
+        output_dir=args.output_dir,
+        overwrite_output_dir=True,
+        evaluation_strategy="epoch",
+        learning_rate=2e-5,
+        weight_decay=0.01,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        push_to_hub=False,
+        fp16=True,
+        logging_steps=logging_steps,
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=lm_datasets["train"],
+        eval_dataset=lm_datasets["test"],
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+    )
+    eval_results = trainer.evaluate()
+    print(f">>> Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
+    trainer.train()
+    eval_results = trainer.evaluate()
+    print(f">>> Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
+
+
