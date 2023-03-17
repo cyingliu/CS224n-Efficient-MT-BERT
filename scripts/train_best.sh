fb17f97927520b3893c3497bb31e9f70701ebd90
Jeffery Shen <jefferyshen@MacBook-Air-95.local>
2023-03-16 22:15:28 -0700

    pal+prefix config

--- scripts/train_best.sh
+++ scripts/train_best.sh
@@ -1,6 +1,6 @@
 python multitask_classifier.py --option finetune --use_gpu\
-	--output_dir result/allconfigSICK\
-    --epochs 25 --lr 1e-5 --lr_adapt 1e-5 --warmup_portion 0.1\
+	--output_dir result/PAL_prefix_SICK\
+    --epochs 15 --lr 1e-5 --lr_adapt 1e-5 --warmup_portion 0.1\
     --batch_size 16 --steps_per_epoch 2400 --eval_interval 4\
     --gradient_accumulation_step 1\
     --hidden_dropout_prob 0.1\
@@ -9,4 +9,4 @@ python multitask_classifier.py --option finetune --use_gpu\
     --similarity_classifier_type 'cosine-similarity'\
     --pooling_type 'mean'\
     --classification_concat_type 'add-abs'\
-    --config_path config/all_config.json
+    --config_path config/pal_prefix_config.json
